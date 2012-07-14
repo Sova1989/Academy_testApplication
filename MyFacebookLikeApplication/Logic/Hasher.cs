2139de0edf9dfc816152bf504a3a3308e40bef52
Sova1989 <ann-sov@yandex.ru>
2012-07-14 16:08:30 +0300

    Made a possibility to register and login using Default MembershipProvider

---
+++ MyFacebookLikeApplication/Logic/Hasher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace MyFacebookLikeApplication.Logic
+{
+    public class Hasher
+    {
+        public string GetHashString(string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            MD5CryptoServiceProvider CSP = new MD5CryptoServiceProvider();
+            byte[] byteHash = CSP.ComputeHash(bytes);
+            StringBuilder hash = new StringBuilder();
+
+            foreach (byte b in byteHash)
+                hash.Append(string.Format("{0:x2}", b));
+
+            return hash.ToString();
+        }  
+    }
+}
---
+++ MyFacebookLikeApplication/RegisterModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFacebookLikeApplication.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your surname")]
+        public string UserSurname { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please choose your gender")]
+        public bool Gender { get; set; }
+
+        [Required(ErrorMessage = "Please write the date of your birth")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "Please choose your marital status")]
+        public bool? MaritalStatus { get; set; }
+
+        public string ListOfInterests { get; set; }
+    }
+}
